a7f85c73964f4806fa8b0b8a77ca177f53ae6728
Jess Kenney <seveneightn9ne@gmail.com>
2016-04-18 00:17:59 -0400

    make goaway.py kinda like an api

---
+++ examples.py
@@ -0,0 +1,18 @@
+""" Example uses of goaway. Cases to remember to handle."""
+from goaway import *
+
+init_master()
+
+""" Non-global reference """
+def f():
+    def g():
+        def h():
+            return "hi"
+        print h()
+    goaway(g)
+
+""" Lambda expression """
+# Maybe we don't need this
+# The lambda wouldn't make sense to take args
+# So it could always be emulated by passing the args to goaway
+goaway(lambda: myobj.do_thing("args"))
--- goaway.py
+++ goaway.py
@@ -1,54 +1,16 @@
-# Echo client program
-import socket
-import sys
-import pickle
-
-class FirstClassValue:
-    def __init__(self, function, name):
-        self.function = function
-        self.name = name
-
-HOST = 'localhost'    # The remote host
-PORT = 50007              # The same port as used by the server
-s = None
-if __name__ == "main":
-    print "Hello"
-    for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM):
-        af, socktype, proto, canonname, sa = res
-        try:
-            s = socket.socket(af, socktype, proto)
-        except socket.error as msg:
-            s = None
-            continue
-        try:
-            s.connect(sa)
-        except socket.error as msg:
-            s.close()
-            s = None
-            continue
-        break
-    if s is None:
-        print 'could not open socket'
-        sys.exit(1)
-    #s.sendall('Hello, world')
-    #file = open('injection.py', 'r')
-    #s.sendall(file.read())
-    #data = s.recv(1024)
-    #print 'Received', repr(data)
+import inspect
 
 ## User runs this function to start off system
 ## probably will read the config file for info
 ## on cluster config, and maybe imports
-def init():
-    ## TODO:
+def init_master():
+    pass
 
-def run(function, name):
-    ## TODO: needs to interpret arguments
-    print function
-    exec(function)
-    exec(name+"()")
-    package = {}
-    package["function"] = function
-    package["name"] = name
-    pickleString = pickle.dumps(package, -1)
-    s.sendall(pickleString)
+def init_slave():
+    pass
+
+def goaway(fn, *args, **kwargs):
+    """ fn is a function, not a string. Its args are *args & **kwargs """
+    source = inspect.getsource(fn)
+    name = fn.__name__
+    print name + " is going away...."
