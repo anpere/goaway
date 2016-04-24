e33f742dc890044f29aecbf363b54b97c894dd94
Andres Perez <andres15perez@gmail.com>
2016-04-23 20:07:28 -0400

    made function/demo better

--- misc/pickleFunction.py
+++ misc/pickleFunction.py
@@ -10,9 +10,11 @@ def printer(string):
 
 def genericTest(function):
     pickledFunction = pickle.dumps(function)
+    print "pickled-function", pickledFunction
     unpickledFunction = pickle.loads(pickledFunction)
     for attribute in dir(function):
         assert getattr(function, attribute) == getattr(unpickledFunction, attribute)
+        print attribute, getattr(function, attribute)
 functions = [printer]
 if __name__ == "__main__":
     for function in functions:
