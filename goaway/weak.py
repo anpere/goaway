7cba30b3beb57772b4dc343e347968015eeb9b8c
Andres Perez <andres15perez@gmail.com>
2016-04-30 00:28:31 -0400

    added server debug info to ignore

--- goaway/weak.py
+++ goaway/weak.py
@@ -1,7 +1,7 @@
 class Weak(object):
     """
     Represents shared memory with weak consistencies
-
+    On creation this data is shared with other machines
     Clients can set fields in this object and use them as
     they want.
     For example:
@@ -10,3 +10,5 @@ class Weak(object):
         zoobars.clients["alyssa"] = 10
 
     """
+    def __init__(self):
+        pass
