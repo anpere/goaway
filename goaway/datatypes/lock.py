1b1a2d93fbc70e0987e8874a4d729e3b993fa44d
Jess Kenney <seveneightn9ne@gmail.com>
2016-05-04 00:18:36 -0400

    lock uses rpc

--- goaway/datatypes/lock.py
+++ goaway/datatypes/lock.py
@@ -1,28 +1,35 @@
 """ a goaway lock """
 import uuid
 import thread
+import goaway.rpc as rpc
 import goaway.globalvars as globalvars
 
 class Lock(object):
     def __init__(self, name):
         self.uuid = uuid.uuid1() # Unique to the process (who is holding the lock)
         self.name = name # All acquires happen on the same name
+        self.acquired = False # Whether I've currently got the lock
 
     def acquire(self):
         """ blocks """
+        if self.acquired:
+            return True # reentrant
         data = {"uuid": self.uuid,
                 "name": self.name}
         while True:
-            resj = self._rpc("POST", self._master_url("lock/acquire"), data)
-            if resj["ok"] == "true": #TODO sketchy string
+            resj = rpc.rpc("POST", rpc.master_url("lock/acquire"), data)
+            if resj["ok"] == "ok": #TODO sketchy string
+                self.acquired = True
                 return
 
     def release(self):
         """ doesn't block """
-        thread.start_new_thread(self._release_async())
+        if self.acquired:
+            self.acquired = False
+            thread.start_new_thread(self._release_async())
 
     def _release_async(self):
         """ sends release notice """
         data = {"uuid": self.uuid,
                 "name": self.name}
-        resj = self._rpc("POST", self._master_url("lock/release"), data)
+        resj = rpc.rpc("POST", rpc.master_url("lock/release"), data)
