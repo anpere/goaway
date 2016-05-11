b51a24ad3c4581cc1b58214682dfc452a57b5217
Andres Perez <andres15perez@gmail.com>
2016-05-11 13:12:14 -0400

    create template for updateonrelease

--- goaway/objectconstructors.py
+++ goaway/objectconstructors.py
@@ -8,3 +8,7 @@ logger = logging.getLogger(__name__)
 class StrictCentralized(ObjectHandle):
     def __init__(self, name):
         ObjectHandle.__init__(self, globalvars.STRICT_CENTRALIZED_KIND, name)
+
+class UpdateOnRelease(ObjectHandle):
+   def __init__(self, name):
+       ObjectHandle.__init__(self, globalvars.updateOnReleaseStoreHandle)
