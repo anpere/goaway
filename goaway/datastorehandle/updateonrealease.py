c39c0eeabb0e67809b261b64bf4efc1f59d383c4
Andres Perez <andres15perez@gmail.com>
2016-05-11 19:06:18 -0400

    wrote updateonrelease object and datastore

--- goaway/datastorehandle/updateonrealease.py
+++ goaway/datastorehandle/updateonrealease.py
@@ -3,10 +3,48 @@ class UpdateOnReleaseDataStoreHandle(DataStoreHandle):
         Ensures that accesses to the objects contained by this datastore
         satisfy the properties of release
     """
+    Operation = collections.namedtuple("Operation", ["name", "field", "value"])
     def __init__(self):
+        self.data = {}
+        self.locks = {}
+        self.buffers = {}
+        assert globalvars.config != None
 
-    def create(self, name):
+    def acquire(self, name):
+        ## TODO: delete the operations buffer?
+        self.locks[name].acquire()
+
+    def release(self, name):
+        ## 0. Get operations buffer
+        operations = self.buffers[name]
+        ## 1. propogate operations buffer to the rest of the servers
+        payload = {
+            "consistency": "update",
+            "name": name,
+            "operations" : operations,
+        }
+
+        for server_address in globalvars.config.all_other_servers:
+            resj = rpc.rpc("POST", self._server_url(server_address, "/update"), payload)
+        ## 2. clear buffer
+        self.buffers[name] = []
+        ## 3. Release the lock
+        self.locks[name].release()
+
+    def make_objet(self, name):
+        self.locks[name] = Lock(name)
+        self.buffers[name] = []
 
     def get(self, name, field):
+        return self.data[name][field]
 
     def set(self, name, field, value):
+        ## write to that object's local cache
+        self.data[name][field] = value
+        ## log the operation to that object's operation buffer
+        object_buffer = self.buffer[name]
+        object_buffer.append(Operation(name, field, value))
+        self.buffer[name] = object_buffer
+
+    def _server_url(self, server_address, url_subpath):
+        return "http://{}:{}/{}".format(server_address.host, server_address.port, url_subpath)
--- goaway/objectconstructors.py
+++ goaway/objectconstructors.py
@@ -1,5 +1,6 @@
 from objecthandle import ObjectHandle, NAME_ATTR
 from goaway.datastorehandle.strictcentralized import StrictCentralizedDataStoreHandle
+from goaway.datatypes.lock import Lock
 import globalvars
 import logging
 
@@ -8,10 +9,6 @@ logger = logging.getLogger(__name__)
 class StrictCentralized(ObjectHandle):
     def __init__(self, name):
         ObjectHandle.__init__(self, globalvars.STRICT_CENTRALIZED_KIND, name)
-
-class UpdateOnRelease(ObjectHandle):
-   def __init__(self, name):
-       ObjectHandle.__init__(self, globalvars.RELEASE_KIND, name)
 
 class LinFastRead(ObjectHandle):
     def __init__(self, name):
@@ -23,3 +20,33 @@ class Weak(ObjectHandle):
 
     def sync(self):
         globalvars.get_data_store(globalvars.WEAK_KIND).sync(getattr(self, NAME_ATTR))
+
+class UpdateOnRelease(ObjectHandle):
+   def __init__(self, name):
+       ObjectHandle.__init__(self, globalvars.RELEASE_KIND, name)
+    """
+    Represents an object who accesses satisfy release consistency
+    Each object has a lock which is used to cooridinate with copies
+    of that object on different machines.
+    Changes made to the object are propogated when that object's lock has
+    been released
+    """
+    def __init__(self, name):
+        self.__dict__[LOCK_ATTR] = Lock(name)
+        ## buffer which contains the operations
+        self.__dict__[BUFFER_ATTR] = []
+        ObjectHandle.__init__(self, globalvars.RELEASE_KIND, name)
+        store = globalvars.get_data_store(getattr(self, DATA_STORE_HANDLE_KIND_ATTR))
+        store.make_object(name)
+
+    def acquire(self):
+        ## 1. Acquire the object's lock
+        store = globalvars.get_data_store(getattr(self, DATA_STORE_HANDLE_KIND_ATTR))
+        object_name = getattr(self, NAME_ATTR)
+        store.acquire(object_name)
+
+    def release(self):
+        store = globalvars.get_data_store(getattr(self, DATA_STORE_HANDLE_KIND_ATTR))
+        object_name = getattr(self, NAME_ATTR)
+        store.release(object_name)
+>>>>>>> wrote updateonrelease object and datastore
