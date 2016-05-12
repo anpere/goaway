4657b2c481a41b027ad9ef083d26a6077bc24e4a
Jess Kenney <seveneightn9ne@gmail.com>
2016-05-12 02:56:11 -0400

    dict bye

--- goaway/datatypes/dict.py
+++
@@ -1,8 +0,0 @@
-import collections
-class GoawayDict(object):
-    def __init__(self):
-        pass
-    def __getitem__(self, key):
-        pass
-    def __setitem__(self, key, value):
-        pass
